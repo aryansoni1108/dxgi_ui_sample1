7f59ab7f3b16b0fafe3d9b9f536754ff7feef574
Aryan Soni <aryansoni1108@gmail.com>
2019-07-14 22:41:46 +0530

    Basic ui with menu

--- app/src/main/java/com/dji/ux/sample/MainActivity.java
+++ app/src/main/java/com/dji/ux/sample/MainActivity.java
@@ -1,347 +1,80 @@
 package com.dji.ux.sample;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
+import dji.ux.widget.BatteryWidget;
+
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import dji.common.error.DJIError;
-import dji.common.error.DJISDKError;
-import dji.common.useraccount.UserAccountState;
-import dji.common.util.CommonCallbacks;
-import dji.log.DJILog;
-import dji.sdk.sdkmanager.DJISDKManager;
-import dji.sdk.base.BaseComponent;
-import dji.sdk.base.BaseProduct;
-import dji.sdk.sdkmanager.DJISDKInitEvent;
-import dji.sdk.useraccount.UserAccountManager;
-
-/** Main activity that displays three choices to user */
-public class MainActivity extends Activity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
-    private static final String TAG = "MainActivity";
-    private static final String LAST_USED_BRIDGE_IP = "bridgeip";
-    private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
-    private static boolean isAppStarted = false;
-    private DJISDKManager.SDKManagerCallback registrationCallback = new DJISDKManager.SDKManagerCallback() {
-
-        @Override
-        public void onRegister(DJIError error) {
-            isRegistrationInProgress.set(false);
-            if (error == DJISDKError.REGISTRATION_SUCCESS) {
-                loginAccount();
-                DJISDKManager.getInstance().startConnectionToProduct();
-
-                Toast.makeText(getApplicationContext(), "SDK registration succeeded!", Toast.LENGTH_LONG).show();
-            } else {
-
-                Toast.makeText(getApplicationContext(),
-                               "SDK registration failed, check network and retry!",
-                               Toast.LENGTH_LONG).show();
-            }
-        }
-        @Override
-        public void onProductDisconnect() {
-            Toast.makeText(getApplicationContext(),
-                           "product disconnect!",
-                           Toast.LENGTH_LONG).show();
-        }
-        @Override
-        public void onProductConnect(BaseProduct product) {
-            Toast.makeText(getApplicationContext(),
-                           "product connect!",
-                           Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        public void onComponentChange(BaseProduct.ComponentKey key,
-                                      BaseComponent oldComponent,
-                                      BaseComponent newComponent) {
-            Toast.makeText(getApplicationContext(),
-                           key.toString() + " changed",
-                           Toast.LENGTH_LONG).show();
-
-        }
-
-        @Override
-        public void onInitProcess(DJISDKInitEvent event, int totalProcess) {
-
-        }
-    };
-
-    private void loginAccount(){
-        UserAccountManager.getInstance().logIntoDJIUserAccount(this,
-                new CommonCallbacks.CompletionCallbackWith<UserAccountState>() {
-                    @Override
-                    public void onSuccess(final UserAccountState userAccountState) {
-                        Toast.makeText(getApplicationContext(),
-                                "Login Success!",
-                                Toast.LENGTH_LONG).show();
-                    }
-                    @Override
-                    public void onFailure(DJIError error) {
-                        Toast.makeText(getApplicationContext(),
-                                "Login Error!",
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
-
-    }
-
-    public static boolean isStarted() {
-        return isAppStarted;
-    }
-    private static final String[] REQUIRED_PERMISSION_LIST = new String[] {
-        Manifest.permission.VIBRATE,
-        Manifest.permission.INTERNET,
-        Manifest.permission.ACCESS_WIFI_STATE,
-        Manifest.permission.WAKE_LOCK,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.ACCESS_NETWORK_STATE,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.CHANGE_WIFI_STATE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.BLUETOOTH,
-        Manifest.permission.BLUETOOTH_ADMIN,
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.READ_PHONE_STATE,
-        Manifest.permission.RECORD_AUDIO
-    };
-    private static final int REQUEST_PERMISSION_CODE = 12345;
-    private List<String> missingPermission = new ArrayList<>();
-    private EditText bridgeModeEditText;
-    private Button primary_activity_btx;
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    BatteryWidget batteryWidget;
+    ImageView menu_btx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        isAppStarted = true;
-        primary_activity_btx = findViewById(R.id.primary_activity_btx);
-        primary_activity_btx.setOnClickListener(new View.OnClickListener() {
+        batteryWidget = findViewById(R.id.battery_indicator);
+        batteryWidget.initKey();
+        menu_btx=findViewById(R.id.toolbar_menu);
+
+        menu_btx.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,sample.class);
-                startActivity(intent);
+                PopupMenu popup = new PopupMenu(MainActivity.this, view);
+                popup.setOnMenuItemClickListener(MainActivity.this);
+                popup.inflate(R.menu.primary_activity_menu);
+                popup.show();
+
             }
         });
-        findViewById(R.id.complete_ui_widgets).setOnClickListener(this);
-        findViewById(R.id.bt_customized_ui_widgets).setOnClickListener(this);
-        findViewById(R.id.bt_map_widget).setOnClickListener(this);
-        TextView versionText = (TextView) findViewById(R.id.version);
-        versionText.setText(getResources().getString(R.string.sdk_version, DJISDKManager.getInstance().getSDKVersion()));
-        bridgeModeEditText = (EditText) findViewById(R.id.edittext_bridge_ip);
-        bridgeModeEditText.setText(PreferenceManager.getDefaultSharedPreferences(this).getString(LAST_USED_BRIDGE_IP,""));
-        bridgeModeEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH
-                    || actionId == EditorInfo.IME_ACTION_DONE
-                    || event != null
-                    && event.getAction() == KeyEvent.ACTION_DOWN
-                    && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-                    if (event != null && event.isShiftPressed()) {
-                        return false;
-                    } else {
-                        // the user is done typing.
-                        handleBridgeIPTextChange();
-                    }
-                }
-                return false; // pass on to other listeners.
-            }
-        });
-        bridgeModeEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s != null && s.toString().contains("\n")) {
-                    // the user is done typing.
-                    // remove new line characcter
-                    final String currentText = bridgeModeEditText.getText().toString();
-                    bridgeModeEditText.setText(currentText.substring(0, currentText.indexOf('\n')));
-                    handleBridgeIPTextChange();
-                }
-            }
-        });
-        checkAndRequestPermissions();
-    }
-
-    @Override
-    protected void onDestroy() {
-        DJISDKManager.getInstance().destroy();
-        isAppStarted = false;
-        super.onDestroy();
-    }
-
-    /**
-     * Checks if there is any missing permissions, and
-     * requests runtime permission if needed.
-     */
-    private void checkAndRequestPermissions() {
-        // Check for permissions
-        for (String eachPermission : REQUIRED_PERMISSION_LIST) {
-            if (ContextCompat.checkSelfPermission(this, eachPermission) != PackageManager.PERMISSION_GRANTED) {
-                missingPermission.add(eachPermission);
-            }
-        }
-        // Request for missing permissions
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else {
-            ActivityCompat.requestPermissions(this,
-                                              missingPermission.toArray(new String[missingPermission.size()]),
-                                              REQUEST_PERMISSION_CODE);
-        }
-    }
-
-    /**
-     * Result of runtime permission request
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Check for granted permission and remove from missing list
-        if (requestCode == REQUEST_PERMISSION_CODE) {
-            for (int i = grantResults.length - 1; i >= 0; i--) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    missingPermission.remove(permissions[i]);
-                }
-            }
-        }
-        // If there is enough permission, we will start the registration
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else {
-            Toast.makeText(getApplicationContext(), "Missing permissions! Will not register SDK to connect to aircraft.", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void startSDKRegistration() {
-        if (isRegistrationInProgress.compareAndSet(false, true)) {
-            AsyncTask.execute(new Runnable() {
-                @Override
-                public void run() {
-                    DJISDKManager.getInstance().registerApp(MainActivity.this, registrationCallback);
-                }
-            });
-        }
-    }
-
-    @Override
-    public void onClick(View view) {
-        Class nextActivityClass;
-
-        int id = view.getId();
-        if (id == R.id.complete_ui_widgets) {
-            nextActivityClass = CompleteWidgetActivity.class;
-        } else if (id == R.id.bt_customized_ui_widgets) {
-            nextActivityClass = CustomizedWidgetsActivity.class;
-        } else {
-            //nextActivityClass = MapWidgetActivity.class;
-            PopupMenu popup = new PopupMenu(this, view);
-            popup.setOnMenuItemClickListener(this);
-            Menu popupMenu = popup.getMenu();
-            MenuInflater inflater = popup.getMenuInflater();
-            inflater.inflate(R.menu.map_select_menu, popupMenu);
-            popupMenu.findItem(R.id.here_map).setEnabled(isHereMapsSupported());
-            popupMenu.findItem(R.id.google_map).setEnabled(isGoogleMapsSupported(this));
-            popup.show();
-            return;
-        }
-
-        Intent intent = new Intent(this, nextActivityClass);
-        startActivity(intent);
     }
 
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
-        Intent intent = new Intent(this, MapWidgetActivity.class);
-        int mapBrand = 0;
-        switch (menuItem.getItemId()) {
-            case R.id.here_map:
-                mapBrand = 0;
+        switch (menuItem.getItemId()){
+            case R.id.system1_menu_item:
+                //TODO: When system 1 item is selected
+                Toast.makeText(MainActivity.this,"System 1",Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.google_map:
-                mapBrand = 1;
+            case R.id.system2_menu_item:
+                //TODO: WHen system 2 item is selected
+                Toast.makeText(MainActivity.this,"System 2",Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.amap:
-                mapBrand = 2;
+            case R.id.system3_menu_item:
+                //TODO: When system3 is selected
+                Toast.makeText(MainActivity.this,"System 3",Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.mapbox:
-                mapBrand = 3;
+            case R.id.system1_save_menu_item:
+                //TODO: When system 1 item is selected
+                Toast.makeText(MainActivity.this,"System 1 create mission",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.system1_create_menu_item:
+                //TODO: When system 1 create item is selected
+                Toast.makeText(MainActivity.this,"System 1 create mission",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.system2_save_menu_item:
+                //TODO: WHen system 2 save mission item is selected
+                Toast.makeText(MainActivity.this,"System 2",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.system2_create_menu_item:
+                //TODO: When system 2 create mission is selected
+                Toast.makeText(MainActivity.this,"System 3",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.system3_save_menu_item:
+                //TODO: WHen system 3 save mission item is selected
+                Toast.makeText(MainActivity.this,"System 2",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.system3_create_menu_item:
+                //TODO: When system 3 create mission is selected
+                Toast.makeText(MainActivity.this,"System 3",Toast.LENGTH_SHORT).show();
                 break;
         }
-        intent.putExtra(MapWidgetActivity.MAP_PROVIDER, mapBrand);
-        startActivity(intent);
         return false;
     }
-
-    public static boolean isHereMapsSupported() {
-        String abi;
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            abi = Build.CPU_ABI;
-        } else {
-            abi = Build.SUPPORTED_ABIS[0];
-        }
-        DJILog.d(TAG, "abi=" + abi);
-
-        //The possible values are armeabi, armeabi-v7a, arm64-v8a, x86, x86_64, mips, mips64.
-        return abi.contains("arm");
-    }
-
-    public static boolean isGoogleMapsSupported(Context context) {
-        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
-        int resultCode = googleApiAvailability.isGooglePlayServicesAvailable(context);
-        return resultCode == ConnectionResult.SUCCESS;
-    }
-
-    private void handleBridgeIPTextChange() {
-        // the user is done typing.
-        final String bridgeIP = bridgeModeEditText.getText().toString();
-
-        if (!TextUtils.isEmpty(bridgeIP)) {
-            DJISDKManager.getInstance().enableBridgeModeWithBridgeAppIP(bridgeIP);
-            Toast.makeText(getApplicationContext(),"BridgeMode ON!\nIP: " + bridgeIP,Toast.LENGTH_SHORT).show();
-            PreferenceManager.getDefaultSharedPreferences(this).edit().putString(LAST_USED_BRIDGE_IP,bridgeIP).apply();
-        }
-    }
 }
+
