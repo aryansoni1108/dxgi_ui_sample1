ed9d98e5742e82648a538bd17aff085d16c7a9c3
Aryan Soni <aryansoni1108@gmail.com>
2019-07-16 23:44:19 +0530

    menus added

--- app/src/main/java/com/dji/ux/sample/MainActivity.java
+++ app/src/main/java/com/dji/ux/sample/MainActivity.java
@@ -1,4 +1,5 @@
 package com.dji.ux.sample;
+import android.media.Image;
 import android.os.Bundle;
 import dji.ux.widget.BatteryWidget;
 
@@ -12,6 +13,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     BatteryWidget batteryWidget;
     ImageView menu_btx;
+    ImageView settings_btx;
+    ImageView ctrl_btx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,26 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         setContentView(R.layout.activity_main);
         batteryWidget = findViewById(R.id.battery_indicator);
         batteryWidget.initKey();
+        settings_btx=findViewById(R.id.settings_img_btx);
+        ctrl_btx=findViewById(R.id.ctrl_btx);
+        ctrl_btx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, view);
+                popup.setOnMenuItemClickListener(MainActivity.this);
+                popup.inflate(R.menu.ctrl_menu);
+                popup.show();
+            }
+        });
+        settings_btx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, view);
+                popup.setOnMenuItemClickListener(MainActivity.this);
+                popup.inflate(R.menu.settings_menu);
+                popup.show();
+            }
+        });
         menu_btx=findViewById(R.id.toolbar_menu);
 
         menu_btx.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +96,19 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
                 //TODO: When system 3 create mission is selected
                 Toast.makeText(MainActivity.this,"System 3",Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.checklist_item:
+                Toast.makeText(MainActivity.this,"Checklist",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cam_setting_item:
+                Toast.makeText(MainActivity.this,"Cam Settings",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.monitor_item:
+                Toast.makeText(MainActivity.this,"monitor",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.send_reports_item:
+                Toast.makeText(MainActivity.this,"Send reports",Toast.LENGTH_SHORT).show();
+                break;
+
         }
         return false;
     }
